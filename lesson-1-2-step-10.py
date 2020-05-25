1f59bbd63e560befeeb0149c73b5f383db445222
Andrey Melostnoy <Andrey.Melostnoy@kaspersky.com>
2020-05-25 23:15:19 +0300

    1-2-10 finished

---
+++ lesson-1-2-step-10.py
@@ -0,0 +1,8 @@
+objects = []
+result = []
+
+for i in objects:
+    if i not in result:
+        result.append(i)
+
+print(len(result))
---
+++ test.py
@@ -0,0 +1,23 @@
+# x = [1, 2, 3]
+# print(id(x))
+# print(type(id(x)))
+# print(id([1, 2, 3]))
+#
+# x = [1, 2, 3]
+# y = x
+# y.append(4)
+#
+# s = "123"
+# t = s
+# t = t + "4"
+#
+# print(str(x) + " " + s)
+
+a = []
+
+def foo(arg1, arg2):
+  a.append("foo")
+
+foo(a.append("arg1"), a.append("arg2"))
+
+print(a)
