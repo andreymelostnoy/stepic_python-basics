a960c2cc90a27219cb7090cbd26b3fc7c025cadf
Andrey Melostnoy <Andrey.Melostnoy@kaspersky.com>
2020-05-30 15:01:59 +0300

    1-3-9 finished

--- test.py
+++ test.py
@@ -12,12 +12,15 @@
 # t = t + "4"
 #
 # print(str(x) + " " + s)
+#
+# a = []
+#
+# def foo(arg1, arg2):
+#   a.append("foo")
+#
+# foo(a.append("arg1"), a.append("arg2"))
+#
+# print(a)
+#
 
-a = []
 
-def foo(arg1, arg2):
-  a.append("foo")
-
-foo(a.append("arg1"), a.append("arg2"))
-
-print(a)
