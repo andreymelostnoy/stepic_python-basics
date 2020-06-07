ed18f3d19c795419e1e13ac5efe50dc2e1c226b9
Andrey Melostnoy <Andrey.Melostnoy@kaspersky.com>
2020-06-07 19:17:55 +0300

    1-6-8 finished

---
+++ lesson-1-6-step-8.py
@@ -0,0 +1,32 @@
+class ExtendedStack(list):
+    def sum(self):
+        if len(self) >= 2:
+            self.append(self.pop() + self.pop())
+
+    def sub(self):
+        if len(self) >= 2:
+            self.append(self.pop() - self.pop())
+
+    def mul(self):
+        if len(self) >= 2:
+            self.append(self.pop() * self.pop())
+
+    def div(self):
+        if len(self) >= 2:
+            self.append(self.pop() // self.pop())
+
+
+def test():
+    ex_stack = ExtendedStack([1, 2, 3, 4, -3, 3, 5, 10])
+    ex_stack.div()
+    assert ex_stack.pop() == 2
+    ex_stack.sub()
+    assert ex_stack.pop() == 6
+    ex_stack.sum()
+    assert ex_stack.pop() == 7
+    ex_stack.mul()
+    assert ex_stack.pop() == 2
+    assert len(ex_stack) == 0
+
+
+test()
