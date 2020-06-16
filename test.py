472731bedd432c3c94b45698de940b6e874b5647
Andrey Melostnoy <Andrey.Melostnoy@kaspersky.com>
2020-06-16 12:36:06 +0300

    2-5-6 finished

--- test.py
+++ test.py
@@ -109,4 +109,25 @@
 # a = filter(None, [1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
 # print(a)
 
-print(3 % 2)
+
+# a = lambda x: x ** 2
+#
+# print(a(8))
+#
+#
+# def make_incrementor(n):
+#     return lambda x: x + n
+#
+#
+# f = make_incrementor(2)
+# g = make_incrementor(6)
+#
+# print(f(42), g(42))
+#
+#
+# foo = [2, 18, 9, 22, 17, 24, 8, 12, 27]
+# print(reduce(lambda x, y: x + y, foo))
+# a = [2, 156, 51]
+# print(a, a.sort(), a)
+
+print(7 % 5)
