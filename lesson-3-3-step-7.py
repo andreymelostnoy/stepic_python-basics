e041840b68b537b6670323dd295740c15c5e89b2
Andrey Melostnoy <Andrey.Melostnoy@kaspersky.com>
2020-06-20 15:26:25 +0300

    3-3-8 finished

--- lesson-3-3-step-7.py
+++ lesson-3-3-step-7.py
@@ -1,8 +1,8 @@
 import re
 import sys
 
+pattern = r"(cat).*?\1"
 for line in sys.stdin:
     line = line.rstrip()
-    pattern = r"(cat).*?\1"
     if re.match(pattern, line) is not None:
         print(line)
