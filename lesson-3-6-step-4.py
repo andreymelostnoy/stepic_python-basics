935727b2de6f578ce09db995b7d51dbbeee41deb
Andrey Melostnoy <Andrey.Melostnoy@kaspersky.com>
2020-06-21 13:20:53 +0300

    3-6-4 finished

---
+++ lesson-3-6-step-4.py
@@ -0,0 +1,23 @@
+import requests
+import json
+
+api_url = "https://api.artsy.net/api/artists/{artist}"
+token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJyb2xlcyI6IiIsInN1YmplY3Rf" \
+        "YXBwbGljYXRpb24iOiI1ZWVmMWIyNDZmOWQxMDAwMGRkNjdhZjEiLCJleHAiOjE1O" \
+        "TMzMzMxNTcsImlhdCI6MTU5MjcyODM1NywiYXVkIjoiNWVlZjFiMjQ2ZjlkMTAwMD" \
+        "BkZDY3YWYxIiwiaXNzIjoiR3Jhdml0eSIsImp0aSI6IjVlZWYxYjI1OTIxMWNhMDA" \
+        "wZWY3MTlkMyJ9.m4Wx-yqnTfQiqqoCPQfdSBDUxLeBfXxrG2CC6jS8dPw"
+headers = {"X-Xapp-Token": token}
+artists = {}
+
+with open("artists.txt", "r", encoding="utf-8") as file:
+    for artist in file:
+        r = requests.get(api_url.format(artist=artist.strip()), headers=headers)
+        j = json.loads(r.text)
+        if r.status_code == 404:
+            pass
+        else:
+            artists[j["sortable_name"]] = j["birthday"]
+
+for artist in sorted(artists.items(), key=lambda para: para[1]):
+    print(artist[0])
